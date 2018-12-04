e5c4b7b6212d04d6e9a8ee691979c641b0d1c6de
Owen <wangluoke007@613.com>
2018-12-04 23:05:37 +0800

    修正了druid配置和redis配置

--- src/main/java/com/ss/bookstore/datasource/DataSourceConfiguration.java
+++ src/main/java/com/ss/bookstore/datasource/DataSourceConfiguration.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,18 +12,80 @@ import org.springframework.context.annotation.Primary;
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import java.sql.SQLException;
+
 @Configuration
 @MapperScan(basePackages={"com.ss.bookstore.mapper"})
 @EnableTransactionManagement
 public class DataSourceConfiguration {
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+    @Value("${spring.datasource.druid.initial-size}")
+    private int initialSize;
+    @Value("${spring.datasource.druid.min-idle}")
+    private int minIdle;
+    @Value("${spring.datasource.druid.max-active}")
+    private int maxActive;
+    @Value("${spring.datasource.druid.max-wait}")
+    private int maxWait;
+    @Value("${spring.datasource.druid.time-between-eviction-runs-millis}")
+    private int timeBetweenEvictionRunsMillis;
+    @Value("${spring.datasource.druid.min-evictable-idle-time-millis}")
+    private int minEvictableIdleTimeMillis;
+    @Value("${spring.datasource.druid.validation-query}")
+    private String validationQuery;
+    @Value("${spring.datasource.druid.test-while-idle}")
+    private boolean testWhileIdle;
+    @Value("${spring.datasource.druid.test-on-borrow}")
+    private boolean testOnBorrow;
+    @Value("${spring.datasource.druid.test-on-return}")
+    private boolean testOnReturn;
+    @Value("${spring.datasource.druid.pool-prepared-statements}")
+    private boolean poolPreparedStatements;
+    @Value("${spring.datasource.druid.max-pool-prepared-statement-per-connection-size}")
+    private int maxPoolPreparedStatementPerConnectionSize;
+    @Value("${spring.datasource.druid.filters}")
+    private String filters;
+    @Value("${spring.datasource.druid.connection-properties}")
+    private String connectionProperties;
+
+
     @Primary
     @Bean("dataSource")
-    @ConfigurationProperties(prefix="spring.datasource")
-    public DataSource dataSource() {
-        DruidDataSource ds = new DruidDataSource();
-        System.out.println("maxActive : "+ds.getMaxActive());
-        return ds ;
+//    @ConfigurationProperties(prefix="spring.datasource")
+    public DataSource dataSource() throws SQLException {
+        DruidDataSource datasource = new DruidDataSource();
+        datasource.setUrl(url);
+        datasource.setUsername(username);
+        datasource.setPassword(password);
+        datasource.setDriverClassName(driverClassName);
+
+        //configuration
+        datasource.setInitialSize(initialSize);
+        datasource.setMinIdle(minIdle);
+        datasource.setMaxActive(maxActive);
+        datasource.setMaxWait(maxWait);
+        datasource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        datasource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        datasource.setValidationQuery(validationQuery);
+        datasource.setTestWhileIdle(testWhileIdle);
+        datasource.setTestOnBorrow(testOnBorrow);
+        datasource.setTestOnReturn(testOnReturn);
+        datasource.setPoolPreparedStatements(poolPreparedStatements);
+        datasource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+        datasource.setFilters(filters);
+        datasource.setConnectionProperties(connectionProperties);
+
+        System.out.println("------maxActive------ : "+datasource.getMaxActive());
+        return datasource ;
     }
+
     //Springboot配置druid时不再需要配置SqlSessionFactory，否则mybatis-plus的BaseMapper分页功能都会失效
 //    @Bean
 //    public SqlSessionFactory sqlSessionFactoryBean() throws Exception{
